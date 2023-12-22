8b8f66c7f85c01a06cf88c4ccaf2982fc2e3cb86
Ifraim Solomonov <mr.ifraim@yandex.ru>
2023-12-22 19:20:42 +0300

    reinit

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -4,10 +4,17 @@ import IFile from "../types/File";
 
 const FileUpload: React.FC = () => {
     const FileUpload: React.FC = () => {
-        const [currentFile, setCurrentFile] = useState<File>();
-        const [progress, setProgress] = useState<number>(0);
-        const [message, setMessage] = useState<string>("");
-        const [fileInfos, setFileInfos] = useState<Array<IFile>>([]);
+    const [currentFile, setCurrentFile] = useState<File>();
+    const [progress, setProgress] = useState<number>(0);
+    const [message, setMessage] = useState<string>("");
+    const [fileInfos, setFileInfos] = useState<Array<IFile>>([]);
+
+    const selectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = event.target;
+        const selectedFiles = files as FileList;
+        setCurrentFile(selectedFiles?.[0]);
+        setProgress(0);
+        };
   
     return (
         
