a57c47c5e659ca128022253f155f085fc805ff88
Ifraim Solomonov <mr.ifraim@yandex.ru>
2023-12-22 19:55:02 +0300

    функционально готов

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import FileUpload from "./components/FileUpload";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ width: "600px" }}>
+      <div className="my-3">
+        <h1>Doc Defender</h1>
+      </div>
+
+      <FileUpload />
     </div>
   );
 }
