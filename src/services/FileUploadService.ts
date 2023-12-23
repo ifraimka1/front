be737603131b8009cadfa98521dbd63e1df5dcb1
Ifraim Solomonov <mr.ifraim@yandex.ru>
2023-12-23 17:37:27 +0300

    подстроился под api

--- src/services/FileUploadService.ts
+++ src/services/FileUploadService.ts
@@ -5,7 +5,7 @@ const upload = (file: File, onUploadProgress: (progressEvent: any) => void): Pro
 
   formData.append("file", file);
 
-  return http.post("/upload", formData, {
+  return http.post("/api/file-upload", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
